8252a1c004ea558725e750a3f0e1d709c166d501
MadFury94 <61389395+MadFury94@users.noreply.github.com>
2022-09-27 21:16:15 +0100

    some changes

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -101,7 +101,7 @@ let categories = ref({
     { title: "Postman", image: "" },
     { title: "Insomnia", image: "" },
     { title: "Microsoft word", image: "" },
-    { title: "Microsoft Powerpoint", image: "" },
+    { title: "Microsoft Power point", image: "" },
   ],
 });
 
